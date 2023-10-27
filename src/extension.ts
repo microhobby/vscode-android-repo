5d9b7093aeddcca4a2177310d8ce981a1cbadf10
Matheus Castello <matheus@castello.eng.br>
2023-10-27 09:27:23 -0300

    package: Add linter rules
    Signed-off-by: Matheus Castello <matheus@castello.eng.br>

--- src/extension.ts
+++ src/extension.ts
@@ -4,23 +4,26 @@ import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate (context: vscode.ExtensionContext): void {
+    console
+        // eslint-disable-next-line max-len
+        .log('Congratulations, your extension "vscode-android-repo" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-android-repo" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    const disposable = vscode.commands.registerCommand(
+        'vscode-android-repo.helloWorld',
+        () => {
+            void vscode.window
+                .showInformationMessage(
+                    'Hello World from vscode-android-repo!'
+                );
+        }
+    );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-android-repo.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-android-repo!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate (): void { }
